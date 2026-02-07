2cdc9e995b3101a57c3e7d57fe51d1d230256a42
MitroxYT <168591279+MitroxYT@users.noreply.github.com>
2026-02-07 22:16:12 +0300

    Update StandartRotation.java

--- src/main/java/thunder/hack/features/modules/combat/aura/impl/StandartRotation.java
+++ src/main/java/thunder/hack/features/modules/combat/aura/impl/StandartRotation.java
@@ -7,6 +7,8 @@ import net.minecraft.util.math.Vec3d;
 import thunder.hack.features.modules.combat.aura.api.AuraUtil;
 import thunder.hack.features.modules.combat.aura.api.RotationHandler;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import static thunder.hack.core.manager.IManager.mc;
 
 public class StandartRotation implements RotationHandler {
@@ -19,13 +21,14 @@ public class StandartRotation implements RotationHandler {
         float yawDelta = (float) MathHelper.wrapDegrees(yawToTarget - currentRotation.x);
         float pitchDelta = (float) MathHelper.wrapDegrees(pitchToTarget - currentRotation.y);
         float yaw = (float) Math.min(Math.max(Math.abs(yawDelta), 0.3), 9999.0f);
-        float pitch = (float) Math.min(Math.max(Math.abs(pitchDelta), 0.4), 9999.0f);
+        float pitch = (float) Math.min(Math.max(Math.abs(pitchDelta), 0.4) + + ThreadLocalRandom.current().nextFloat(0.2F,1.990F), 9999.0f);
         float yaw33 = (float) (currentRotation.x + (yawDelta > 1.6f ? yaw : -yaw));
         float gcd = (float) (currentRotation.x + (yawDelta > 1.0f ? yaw : -yaw));
         pitch = (float) MathHelper.clamp(currentRotation.y + (pitchDelta > 1.0f ? pitch : -pitch), -91, 89);
         double gcdFix = (Math.pow(mc.options.getMouseSensitivity().getValue() * 0.6 + 0.2, 3.0)) * 1.2;
-        gcd -= (yaw33 - currentRotation.x) % gcdFix;
-        pitch -= (pitch - currentRotation.y) % gcdFix;
+       // System.out.println("aaa");
+        gcd -= (float) ((yaw33 - currentRotation.x) % gcdFix) + ThreadLocalRandom.current().nextFloat(0.95F,9.990F);
+        pitch -= (float) ((pitch - currentRotation.y) % gcdFix);
         return new Vec3d(gcd, pitch,0);
     }
 
